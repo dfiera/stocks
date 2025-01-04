4beaeeeb9ff144933d0a0305cb1a4043efdb2a86
Dragos Fiera <ddfiera@gmail.com>
2025-01-04 13:51:52 +0200

    frontend: add Markets page UI

--- apps/frontend/src/components/Markets.tsx
+++ apps/frontend/src/components/Markets.tsx
@@ -1,15 +1,18 @@
-import Portfolio from './Portfolio.tsx'
-import { Card } from './Card.tsx'
+import MarketSentiment from './MarketSentiment.tsx';
+import MarketMovers from './MarketMovers.tsx';
+import Watchlist from './Watchlist.tsx'
+import MarketTrends from './MarketTrends.tsx';
+import SectorPerformance from './SectorPerformance.tsx';
 
 export default function Markets() {
   return (
     <>
-      <div className="grid grid-cols-3 gap-4">
-        <Portfolio />
-        <Card className="dark:text-white">Market News and Sentiment </Card>
-        <Card className="dark:text-white">Top Gainers, Losers, and Most Actively Traded Tickers</Card>
-
-        <Card className="dark:text-white col-span-3">WIP</Card>
+      <div className="grid grid-cols-[1fr,auto,1.1fr] grid-rows-[auto,auto,1fr] gap-4 h-full">
+        <MarketSentiment />
+        <MarketMovers />
+        <Watchlist />
+        <MarketTrends />
+        <SectorPerformance />
       </div>
     </>
   )
