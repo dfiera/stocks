f863dc94ac3028ecd90a0b756c8ebabee126b08b
Dragos Fiera <ddfiera@gmail.com>
2025-01-26 23:56:43 +0200

    backend: improve redis error handling and enable graceful server shutdown

--- apps/backend/src/redis/redis.ts
+++ apps/backend/src/redis/redis.ts
@@ -5,11 +5,14 @@ export const REDIS_STORE_PREFIX = 'session:';
 
 export const redisClient = new Redis({
   host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT || 6379,
-  password: process.env.REDIS_PASSWORD,
+  port: process.env.REDIS_PORT || 6379
 });
 
 export const redisStore = new RedisStore({
   client: redisClient,
   prefix: REDIS_STORE_PREFIX
+});
+
+redisClient.on('error', (err) => {
+  console.error('Redis error:', err);
 });
--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -2,10 +2,39 @@ import { createServer } from 'node:http';
 import app from './app.ts';
 import { setupSocketIO } from './socket/socket.ts';
 
+import sql from './db/index.ts';
+import { redisClient } from './redis/redis.ts';
+
 const server = createServer(app);
 const PORT = process.env.PORT || 3000;
 
+const gracefulShutdown = async () => {
+  console.log('Shutting down the server...');
+
+  try {
+    await sql.end();
+    console.log('Database connection closed');
+  } catch (error) {
+    console.error('Error closing database connection', error);
+  }
+
+  try {
+    await redisClient.quit();
+    console.log('Redis connection closed');
+  } catch (error) {
+    console.error('Error closing Redis connection', error);
+  }
+
+  server.close(() => {
+    console.log('Server shut down successfully');
+    process.exit(0);
+  });
+};
+
 setupSocketIO(server);
+
+process.on('SIGINT', gracefulShutdown);
+process.on('SIGTERM', gracefulShutdown);
 
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
